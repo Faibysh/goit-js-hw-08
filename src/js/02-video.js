7674f87b383fa5a4f27c44963b6730aea749d856
Faibysh <faybysh.b@gmail.com>
2023-01-20 11:28:16 +0100

    Finished task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+      player.on('play', function() {
+    });
+
+      player.getVideoTitle().then(function(title) {
+    });
+
+
+// player.on('timeupdate', throttle (function () {
+//     player.getCurrentTime().then(function (seconds) {
+//         save("videoplayer-current-time", JSON.stringify(seconds));
+//     });
+// }));
+
+
+    
+    const TIME_DATA_PLAYER = 'videoplayer-current-time';
+      player.on("timeupdate", throttle(function (data) {
+     const time = data.seconds;
+      localStorage.setItem(TIME_DATA_PLAYER, time)},1000));
+    
+    const CURRENT_TIME = 'videoplayer-current-time';
+    const time = +localStorage.getItem(CURRENT_TIME);
+      player.setCurrentTime(time);
+      document.addEventListener('touchstart', { passive: true });
