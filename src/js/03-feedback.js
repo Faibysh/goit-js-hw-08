5338e064f4b05815cc82d123f4fb5b1b0e144cf1
Faibysh <faybysh.b@gmail.com>
2023-01-22 14:02:56 +0100

    fixed after comment

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,13 +11,15 @@ feedbackForm.addEventListener("submit", submitForm);
 function submitForm(evt) {
   evt.preventDefault();
   evt.target.reset();
+  console.log(localObject);
+  localObject = {email: '', message: ''}
   localStorage.removeItem("feedback-form-state");
 }
 
 feedbackForm.addEventListener("input", throttle(addLocalStorage, 500));
 
 function addLocalStorage(evt) {
-   localObject [evt.target.name] = evt.target.value;
+   localObject[evt.target.name] = evt.target.value;
    localStorage.setItem("feedback-form-state", JSON.stringify(localObject));
 } 
 
@@ -26,9 +28,11 @@ const data = localStorage.getItem("feedback-form-state");
 function insertLocalStorage (object) {
   if (object && emailInput.name === "email") {
     emailInput.value = JSON.parse(object).email;
+    localObject.email = JSON.parse(object).email
   }
   if (object && messageInput.name === "message") {
     messageInput.value = JSON.parse(object).message;
+    localObject.message = JSON.parse(object).message
   }
 }
 insertLocalStorage (data); 
